7b2df5841cff21d64ec7c3c3544a9971341a76d7
seric6341 <115032856+seric6341@users.noreply.github.com>
2022-12-16 22:04:57 -0500

    Initial commit

---
+++ lab2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "lab2.h"
+
+
+int** read_board_from_file(char* filename){
+    FILE *fp = NULL;
+    int** board = NULL;
+
+    // replace this comment with your code
+
+    return board;
+}
+
+
+int is_board_valid(){
+    pthread_t* tid;  /* the thread identifiers */
+    pthread_attr_t attr;
+    param_struct* parameter;
+    
+    // replace this comment with your code
+    
+}
